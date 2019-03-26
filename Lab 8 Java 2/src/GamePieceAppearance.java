43ba9b1e64d291ab49ff85603859ce99ff04715d
Derek Ford <derekford@ou.edu>
2019-03-25 23:07:19 -0500

    Created variables, constructor, and getters of Game Piece Appearance

--- Lab 8 Java 2/src/GamePieceAppearance.java
+++ Lab 8 Java 2/src/GamePieceAppearance.java
@@ -1,4 +1,48 @@
 
-public class GamePieceAppearance {
+/**
+ * A  simple  class  that  acts  as  a  composite  data  structure  of  a  Color  and  Shape. 
+ * In effect, holds information on the appearance of a game piece.
+ *
+ */
+public class GamePieceAppearance 
+{
+	/**
+	 * Holds the color of the Game Piece
+	 */
+	private Color color;
+	
+	/**
+	 * Holds the Shape of the Game Piece
+	 */
+	private Shape shape;
+	
+	/**
+	 * Constructor that assigns color and shape of specific Game Piece
+	 * @param color
+	 * @param shape
+	 */
+	public GamePieceAppearance(Color color, Shape shape)
+	{
+		this.color = color;
+		this.shape = shape;
+	}
 
+	/**
+	 * Returns the color of the Game Piece
+	 * @return color
+	 */
+	public Color getColor() 
+	{
+		return color;
+	}
+
+	/**
+	 * Returns the shape of the Game Piece
+	 * @return shape
+	 */
+	public Shape getShape() 
+	{
+		return shape;
+	}
+	
 }
