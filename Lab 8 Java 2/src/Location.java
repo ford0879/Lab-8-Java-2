ad23893b1bac4cdd4459d557babea138f74e40ac
Derek Ford <derekford@ou.edu>
2019-03-25 22:58:45 -0500

    Completed Location enum

--- Lab 8 Java 2/src/Location.java
+++ Lab 8 Java 2/src/Location.java
@@ -1,4 +1,10 @@
 
-public class Location {
-
+/**
+ * Specifies the locations that game pieces can move to in the board game (think of Clue, for example).\
+ *   Multiple game pieces may be at the same location.
+ *
+ */
+public enum Location
+{
+	KITCHEN, CONSERVATORY, DINING_ROOM, BALLROOM, STUDY, HALL, LOUNGE, LIBRARY, BILLIARD_ROOM;
 }
